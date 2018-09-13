273f841077a26a098de18f356d11d67caa0a3d08
Travis T <travistokuyama@gmail.com>
2018-09-13 14:26:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length == 2 
     return "#{array[0]} and #{array[1]}"
   elsif array.length > 2 
-    array[-1].insert("and")
+    array.insert("and")
   array.join(", ")
 end
